60e05679f9700143601cfe65114441d2be8878f1
ra334 <savcukmihajlo@gmail.com>
2025-01-19 22:01:43 +0200

    create user model

---
+++ db/models/user.d.ts
@@ -0,0 +1,132 @@
+interface UserData {
+    id: string;
+    firstName: string;
+    lastName: string;
+    nickName: string;
+    email: string;
+    password: string;
+    avatarURL: string;
+    status: 'offline' | 'online' | 'away';
+    isVerified: boolean;
+    isReported: boolean;
+    isActive: boolean;
+    isBlocked: boolean;
+    createdAT: Date;
+    updatedAT: Date;
+}
+
+interface CreateUser {
+    firstName: string;
+    lastName: string;
+    nickName: string;
+    email: string;
+    password: string;
+    avatarURL: string;
+}
+
+interface GetUser {
+    id: string;
+}
+
+interface ReturnUser {
+    id: string;
+    firstName: string;
+    lastName: string;
+    nickName: string;
+    email: string;
+    avatarURL: string;
+}
+
+interface ReturnUserFirstName {
+    firstName: string;
+}
+
+interface ReturnUserAvatarURL {
+    avatarURL: string;
+}
+
+interface UpdateUserFirstName {
+    id: string;
+    firstName: string;
+}
+
+interface UpdateUserLastName {
+    id: string;
+    lastName: string;
+}
+
+interface UpdateUserNickname {
+    id: string;
+    nickName: string;
+}
+
+interface UpdateUserEmail {
+    id: string;
+    email: string;
+}
+
+interface UpdateUserPassword {
+    id: string;
+    password: string;
+}
+
+interface UpdateUserAvatarURL {
+    id: string;
+    avatarURL: string;
+}
+
+interface UpdateUserStatus {
+    id: string;
+    status: "offline" | "online" | "away";
+}
+
+interface UpdateUserIsVerified {
+    id: string;
+    isVerified: boolean;
+}
+
+interface UpdateUserIsReported {
+    id: string;
+    isReported: boolean;
+}
+
+interface UpdateUserIsActive {
+    id: string;
+    isActive: boolean;
+}
+
+interface UpdateUserIsBlocked {
+    id: string;
+    isBlocked: boolean;
+}
+
+interface UpdateUserUpdateAt {
+    id: string;
+    updated_at: Date;
+}
+
+interface DeleteUser {
+    id: string;
+}
+
+export {
+    UserData,
+    CreateUser,
+    GetUser,
+    ReturnUser,
+    ReturnUserFirstName,
+    ReturnUserAvatarURL,
+    UpdateUserFirstName,
+    UpdateUserLastName,
+    UpdateUserNickname,
+    UpdateUserEmail,
+    UpdateUserPassword,
+    UpdateUserAvatarURL,
+    UpdateUserStatus,
+    UpdateUserIsVerified,
+    UpdateUserIsReported,
+    UpdateUserIsActive,
+    UpdateUserIsBlocked,
+    UpdateUserUpdateAt,
+    DeleteUser
+}
---
+++ db/models/users-model.ts
@@ -0,0 +1,320 @@
+import { db } from '@/db/postgress'
+import { users } from '@/db/schema/users'
+import { eq } from 'drizzle-orm'
+import {
+    UserData,
+    CreateUser,
+    GetUser,
+    ReturnUser,
+    ReturnUserFirstName,
+    ReturnUserAvatarURL,
+    UpdateUserFirstName,
+    UpdateUserLastName,
+    UpdateUserNickname,
+    UpdateUserEmail,
+    UpdateUserPassword,
+    UpdateUserAvatarURL,
+    UpdateUserStatus,
+    UpdateUserIsVerified,
+    UpdateUserIsReported,
+    UpdateUserIsActive,
+    UpdateUserIsBlocked,
+    UpdateUserUpdateAt,
+    DeleteUser
+} from './user'
+
+// create
+
+async function createUser(data: CreateUser): Promise<UserData> {
+    try {
+        const newUser = await db.insert(users).values({
+            firstName: data.firstName,
+            lastName: data.lastName,
+            nickName: data.nickName,
+            email: data.email,
+            password: data.password,
+            avatarURL: data.avatarURL,
+        }).returning()
+
+        return newUser[0]
+    } catch (error) {
+        console.error('Creating a user error:', error)
+        throw error;
+    }
+}
+
+// read
+
+async function getUser(data: GetUser): Promise<ReturnUser> {
+    try {
+        const user = await db.query.users.findFirst({
+            where: (eq(users.id, data.id)),
+            columns: {
+                id: true,
+                firstName: true,
+                lastName: true,
+                nickName: true,
+                email: true,
+                avatarURL: true,
+            }
+        })
+
+        if (!user) {
+            throw new Error('User not found');
+        }
+
+        return user;
+    } catch (error) {
+        console.error('Getting a user error:', error)
+        throw error;
+    }
+}
+
+async function getUserFirstName(data: GetUser): Promise<ReturnUserFirstName> {
+    try {
+
+        const firstName = await db.query.users.findFirst({
+            where: (eq(users.id, data.id)),
+            columns: {
+                firstName: true
+            }
+        })
+
+        if (!firstName) {
+            throw new Error('User not found');
+        }
+
+        return firstName
+
+    } catch (error) {
+        console.error('Getting a user first name error:', error)
+        throw error;
+    }
+}
+
+async function getUserAvatarURL(data: GetUser): Promise<ReturnUserAvatarURL> {
+    try {
+
+        const avatarURL = await db.query.users.findFirst({
+            where: (users, { eq }) => eq(users.id, data.id),
+            columns: {
+                avatarURL: true
+            }
+        })
+
+        if (!avatarURL) {
+            throw new Error('User not found');
+        }
+
+        return avatarURL
+
+    } catch (error) {
+        console.error('Getting a user avatar URL error:', error)
+        throw error;
+    }
+}
+
+// update
+
+async function updateUserFirstName(data: UpdateUserFirstName)  {
+    try {
+        const updatedUser = await db
+            .update(users)
+            .set({firstName: data.firstName})
+            .where(eq(users.id, data.id))
+
+        return updatedUser
+    } catch (error) {
+        console.error('Updating a user first name error:', error)
+        throw error;
+    }
+}
+
+async function updateUserLastName(data: UpdateUserLastName) {
+    try {
+        const updatedUser = await db
+            .update(users)
+            .set({lastName: data.lastName})
+            .where(eq(users.id, data.id))
+
+        return updatedUser
+    } catch (error) {
+        console.error('Updating a user last name error:', error)
+        throw error;
+    }
+}
+
+async function updateUserNickname(data: UpdateUserNickname) {
+    try {
+        const updatedUser = await db
+            .update(users)
+            .set({nickName: data.nickName})
+            .where(eq(users.id, data.id))
+
+        return updatedUser
+    } catch (error) {
+        console.error('Updating a user nickname error:', error)
+        throw error;
+    }
+}
+
+async function updateUserEmail(data: UpdateUserEmail) {
+    try {
+        const updatedUser = await db
+            .update(users)
+            .set({email: data.email})
+            .where(eq(users.id, data.id))
+
+        return updatedUser
+    } catch (error) {
+        console.error('Updating a user email error:', error)
+        throw error;
+    }
+}
+
+async function updateUserPassword(data: UpdateUserPassword) {
+    try {
+        const updatedUser = await db
+            .update(users)
+            .set({password: data.password})
+            .where(eq(users.id, data.id))
+
+        return updatedUser
+    } catch (error) {
+        console.error('Updating a user password error:', error)
+        throw error;
+    }
+}
+
+async function updateUserAvatarURL(data: UpdateUserAvatarURL) {
+    try {
+        const updatedUser = await db
+            .update(users)
+            .set({avatarURL: data.avatarURL})
+            .where(eq(users.id, data.id))
+
+        return updatedUser
+    } catch (error) {
+        console.error('Updating a user avatar URL error:', error)
+        throw error;
+    }
+}
+
+async function updateUserStatus(data: UpdateUserStatus) {
+    try {
+        const updatedUser = await db
+            .update(users)
+            .set({status: data.status})
+            .where(eq(users.id, data.id))
+
+        return updatedUser
+    } catch (error) {
+        console.error('Updating a user status error:', error)
+        throw error;
+    }
+}
+
+async function updateUserIsVerified(data: UpdateUserIsVerified) {
+    try {
+        const updatedUser = await db
+            .update(users)
+            .set({isVerified: data.isVerified})
+            .where(eq(users.id, data.id))
+
+        return updatedUser
+    } catch (error) {
+        console.error('Updating a user is_verified error:', error)
+        throw error;
+    }
+}
+
+async function updateUserIsReported(data: UpdateUserIsReported) {
+    try {
+        const updatedUser = await db
+            .update(users)
+            .set({isReported: data.isReported})
+            .where(eq(users.id, data.id))
+
+        return updatedUser
+    } catch (error) {
+        console.error('Updating a user is_reported error:', error)
+        throw error;
+    }
+}
+
+async function updateUserIsActive(data: UpdateUserIsActive) {
+    try {
+        const updatedUser = await db
+            .update(users)
+            .set({isActive: data.isActive})
+            .where(eq(users.id, data.id))
+
+        return updatedUser
+    } catch (error) {
+        console.error('Updating a user is_active error:', error)
+        throw error;
+    }
+}
+
+async function updateUserIsBlocked(data: UpdateUserIsBlocked) {
+    try {
+        const updatedUser = await db
+            .update(users)
+            .set({isBlocked: data.isBlocked})
+            .where(eq(users.id, data.id))
+
+        return updatedUser
+    } catch (error) {
+        console.error('Updating a user is_blocked error:', error)
+        throw error;
+    }
+}
+
+async function updateUserUpdatedAt(data: UpdateUserUpdateAt) {
+    try {
+        const updatedUser = await db
+            .update(users)
+            .set({updatedAT: new Date()})
+            .where(eq(users.id, data.id))
+
+        return updatedUser
+    } catch (error) {
+        console.error('Updating a user updated_at error:', error)
+        throw error;
+    }
+}
+
+// delete
+
+async function deleteUser(data: DeleteUser) {
+    try {
+        const deletedUser = await db
+            .delete(users)
+            .where(eq(users.id, data.id))
+
+        return deletedUser
+    } catch (error) {
+        console.error('Deleting a user error:', error)
+        throw error;
+    }
+}
+
+export {
+    createUser,
+    getUser,
+    getUserFirstName,
+    getUserAvatarURL,
+    updateUserFirstName,
+    updateUserLastName,
+    updateUserNickname,
+    updateUserEmail,
+    updateUserPassword,
+    updateUserAvatarURL,
+    updateUserStatus,
+    updateUserIsVerified,
+    updateUserIsReported,
+    updateUserIsActive,
+    updateUserIsBlocked,
+    updateUserUpdatedAt,
+    deleteUser
+}
