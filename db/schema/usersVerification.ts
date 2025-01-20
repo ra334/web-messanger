da2fe825bd093323704cc180d0476fa72c992290
ra334 <savcukmihajlo@gmail.com>
2025-01-20 10:27:18 +0200

    update userVerification schema

--- db/schema/usersVerification.ts
+++ db/schema/usersVerification.ts
@@ -2,8 +2,8 @@ import {pgTable, varchar, timestamp, boolean, uuid } from "drizzle-orm/pg-core";
 import { users } from "./users";
 
 export const userVerifications = pgTable('user_verifications', {
-    userId: uuid('user_id').references(() => users.id),
+    userID: uuid('user_id').notNull().references(() => users.id),
     verificationCode: varchar('verification_code', {length: 45}).notNull(),
-    isUsed: boolean('is_used').default(false),
-    createdAt: timestamp('created_at').defaultNow(),
+    isUsed: boolean('is_used').notNull().default(false),
+    createdAt: timestamp('created_at').notNull().defaultNow(),
 })
