7857f67880167ab2c66014c2e77b543ea1cf2690
ra334 <savcukmihajlo@gmail.com>
2025-01-20 16:48:35 +0200

    update groupMembers schema

--- db/schema/groupMembers.ts
+++ db/schema/groupMembers.ts
@@ -4,10 +4,10 @@ import { users } from "./users";
 
 export const groupsMembers = pgTable('groups_members', {
     id: uuid('id').primaryKey().defaultRandom(),
-    groupId: uuid('group_id').references(() => groups.id),
-    userId: uuid('user_id').references(() => users.id),
-    isAdmin: boolean('is_admin').default(false),
-    joinedAt: timestamp('joined_at').defaultNow(),
-    createdAt: timestamp('created_at').defaultNow(),
-    updatedAt: timestamp('updated_at').defaultNow(),
+    groupID: uuid('group_id').notNull().references(() => groups.id),
+    userID: uuid('user_id').notNull().references(() => users.id),
+    isAdmin: boolean('is_admin').notNull().default(false),
+    joinedAt: timestamp('joined_at').notNull().defaultNow(),
+    createdAt: timestamp('created_at').notNull().defaultNow(),
+    updatedAt: timestamp('updated_at').notNull().defaultNow(),
 })
