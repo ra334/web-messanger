59e13ae231624f0669ea6a757e5e650979ec79a3
ra334 <savcukmihajlo@gmail.com>
2025-01-20 21:11:00 +0200

    update devices schema

--- db/schema/devices.ts
+++ db/schema/devices.ts
@@ -3,12 +3,12 @@ import { users } from "./users";
 
 export const devices = pgTable('devices', {
     id: uuid().primaryKey().defaultRandom(),
-    userId: uuid().references(() => users.id),
+    userID: uuid().notNull().references(() => users.id),
     deviceName: varchar('device_name', {length: 255}).notNull(),
     deviceType: varchar('device_type', {length: 100}).notNull(),
     userAgent: text('user_agent').notNull(),
-    isActive: boolean('is_active').default(true),
-    lastAccessedAt: timestamp('last_accessed_at').defaultNow(),
-    createdAt: timestamp('created_at').defaultNow(),
-    updatedAt: timestamp('updated_at').defaultNow()
+    isActive: boolean('is_active').notNull().default(true),
+    lastAccessedAt: timestamp('last_accessed_at').notNull().defaultNow(),
+    createdAt: timestamp('created_at').notNull().defaultNow(),
+    updatedAt: timestamp('updated_at').notNull().defaultNow()
 })
