1f52afe2400bd58578d36204f370d915786fcaed
ra334 <savcukmihajlo@gmail.com>
2025-01-20 21:21:17 +0200

    update blockedUsers schema

--- db/schema/blockedUsers.ts
+++ db/schema/blockedUsers.ts
@@ -2,7 +2,7 @@ import {pgTable, timestamp, uuid } from "drizzle-orm/pg-core";
 import { users } from "./users";
 
 export const blockedUsers = pgTable('blocked_users', {
-    id: uuid('id').primaryKey().defaultRandom(),
-    blockerID: uuid('blocker_id').references(() => users.id),
-    createdAt: timestamp('created_at').defaultNow(),
+    id: uuid('id').primaryKey().notNull().defaultRandom(),
+    blockedID: uuid('blocker_id').notNull().references(() => users.id),
+    createdAt: timestamp('created_at').notNull().defaultNow(),
 });
