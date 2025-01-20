a5842e212d7c313ba546eb6b8e9a24de5d87d20f
ra334 <savcukmihajlo@gmail.com>
2025-01-20 19:51:36 +0200

    update dialogs schema

--- db/schema/dialogs.ts
+++ db/schema/dialogs.ts
@@ -2,11 +2,11 @@ import {pgTable, varchar, timestamp, boolean, uuid } from "drizzle-orm/pg-core";
 import { users } from "./users";
 
 export const dialogs = pgTable('dialogs', {
-    id: uuid('id').primaryKey().defaultRandom(),
-    senderID: uuid('sended_user_id').references(() => users.id),
-    receiverID: uuid('received_user_id').references(() => users.id),
-    isDeletedForSender: boolean('is_deleted_for_sender').default(false),
-    isDeletedForReceiver: boolean('is_deleted_for_receiver').default(false),
-    lastMessage: varchar('last_message', {length: 150}),
-    createdAt: timestamp('created_at').defaultNow()
+    id: uuid('id').primaryKey().notNull().defaultRandom(),
+    senderID: uuid('sended_user_id').notNull().references(() => users.id),
+    receiverID: uuid('received_user_id').notNull().references(() => users.id),
+    isDeletedForSender: boolean('is_deleted_for_sender').notNull().default(false),
+    isDeletedForReceiver: boolean('is_deleted_for_receiver').notNull().default(false),
+    lastMessage: varchar('last_message', {length: 150}).notNull(),
+    createdAt: timestamp('created_at').notNull().defaultNow()
 })
