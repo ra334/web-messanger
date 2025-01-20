1ccf39df6c213f223c63abdba4284b4ea0f44155
ra334 <savcukmihajlo@gmail.com>
2025-01-20 16:19:09 +0200

    update groups schema

--- db/schema/groups.ts
+++ db/schema/groups.ts
@@ -1,10 +1,12 @@
 import {pgTable, integer, varchar, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import { users } from '@/db/schema/users'
 
 export const groups = pgTable('groups', {
     id: uuid('id').primaryKey().defaultRandom(),
+    creatorID: uuid('creator_id').notNull().references(() => users.id),
     name: text('name').notNull(),
     description: text('description').notNull(),
-    avatarURL: varchar('avatar_url', { length: 255 }),
-    membersCount: integer('members_count').default(0),
-    createdAt: timestamp('created_at').defaultNow(),
+    avatarURL: varchar('avatar_url', { length: 255 }).notNull(),
+    membersCount: integer('members_count').default(0).notNull(),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
 })
