6fa7498c53bff10691c7e5ca2b3f144127445bcc
ra334 <savcukmihajlo@gmail.com>
2025-01-24 11:30:09 +0200

    add schema medias

---
+++ db/schema/medias.ts
@@ -0,0 +1,15 @@
+import {pgTable, bigint, json, varchar, timestamp, uuid, pgEnum } from "drizzle-orm/pg-core";
+import { users } from "./users";
+
+export const mediaType = pgEnum('media_type', ['image', 'video', 'audio', 'file'])
+
+export const medias = pgTable("medias", {
+    id: uuid("id").primaryKey().defaultRandom(),
+    userID: uuid("user_id").references(() => users.id),
+    type: mediaType("type").notNull(),
+    url: varchar("url", {length: 255}).notNull(),
+    fileName: varchar("file_name", {length: 255}).notNull(),
+    size: bigint("size", { mode: "number" }).notNull(),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+    metadata: json("metadata"),
+});
