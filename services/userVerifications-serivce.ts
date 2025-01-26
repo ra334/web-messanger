fb08fd4a4d5b161583471c0b6034f20725c5300f
ra334 <savcukmihajlo@gmail.com>
2025-01-26 14:40:39 +0200

    create userVerifications service

--- services/userVerifications-serivce.ts
+++ services/userVerifications-serivce.ts
@@ -1,4 +1,4 @@
-import userVerification from '@/db/models/usersVerifications/usersVerifications-model'
+import userVerification from '@/db/models/userVerifications/userVerifications-model'
 import {
     UsesrVerification,
     CreateUserVerification,
@@ -6,6 +6,59 @@ import {
     UpdateUserVerification,
     UpdateIsUsed,
     DeleteUserVerification
-} from '@/db/models/usersVerifications/userVerificatons'
+} from '@/db/models/userVerifications/userVerificatons'
 
-class UserVe
+class UserVerificaionsService {
+    async #getUserVerification(data: GetUserVerification): Promise<UsesrVerification> {
+        const verification = await userVerification.getUserVerification(data)
+
+        return verification
+    }
+
+    async #updateIsUsed(data: UpdateIsUsed): Promise<UsesrVerification> {
+        const verification = await userVerification.updateIsUsed(data)
+
+        return verification
+    }
+
+    async #deleteUserVerification(data: DeleteUserVerification): Promise<UsesrVerification> {
+        const verification = await userVerification.deleteUserVerification(data)
+
+        return verification
+
+    }
+    
+
+    // TODO send email
+    async createUserVerification(userID: string) {
+        const verificationCode = Math.floor(100000 + Math.random() * 900000).toString()
+
+        const verification = await userVerification.createUserVerification({userID, verificationCode})
+
+        return verification
+    }
+
+    async validateVerificationCode(userID: string, code: string) {
+        const verification = await this.#getUserVerification({userID})
+
+        if (verification.isUsed) {
+            throw new Error('Verification code is already used')
+        }
+
+        if (verification.verificationCode !== code) {
+            throw new Error('Invalid verification code')
+        }
+
+        this.#updateIsUsed({userID, isUsed: true})
+
+        return verification
+    }
+
+    async updateUserVerificationCode(userID: string, code: string) {
+        const verification = await userVerification.updateVerificationCode({userID, verificationCode: code})
+
+        return verification
+    }
+}
+
+export default new UserVerificaionsService()
