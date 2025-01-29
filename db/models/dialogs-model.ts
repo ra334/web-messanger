5a7a7fd335d05d7aa5bd3f1c4843b55998fc8739
ra334 <savcukmihajlo@gmail.com>
2025-01-29 11:17:05 +0200

    reafactor dialogs-model

--- db/models/dialogs-model.ts
+++ db/models/dialogs-model.ts
@@ -24,8 +24,17 @@ class DialogsModel {
             }).returning()
 
             return dialog[0]
-        } catch (error) {
-            console.error('Creating dialog error:', error)
+        } catch (error: any) {
+            
+            const senderError = 'insert or update on table "dialogs" violates foreign key constraint "dialogs_sended_user_id_users_id_fk"'
+            const receiverError = 'insert or update on table "dialogs" violates foreign key constraint "dialogs_received_user_id_users_id_fk"'
+
+            if (error.message === senderError) {
+                throw new Error('Sender not found')
+            } else if (error.message === receiverError) {
+                throw new Error('Receiver not found')
+            }
+            
             throw error
         }
     }
@@ -33,105 +42,95 @@ class DialogsModel {
     // read
 
     async getDialog(data: GetDialog): Promise<Dialog> {
-        try {
-            const dialog = await db.query.dialogs.findFirst({
-                where: (eq(dialogs.id, data.id))
-            })
+        const dialog = await db.query.dialogs.findFirst({
+            where: (eq(dialogs.id, data.id))
+        })
 
-            if (!dialog) {
-                throw new Error('Dialog not found')
-            }
-
-            return dialog
-        } catch (error) {
-            console.error('Getting dialog error:', error)
-            throw error
+        if (!dialog) {
+            throw new Error('Dialog not found')
         }
+
+        return dialog
     }
 
-    async getDialogs(data: GetDialogs): Promise<Dialog[]> {
-        try {
-            const dialogFromUserID = await db.query.dialogs.findMany({
-                where: (
-                    eq(dialogs.senderID, data.userID),
-                    eq(dialogs.receiverID, data.userID)
-                ),
-            })
+    async getDialogsByUser(data: GetDialogs): Promise<Dialog[]> {
+        const dialogFromUserID = await db.query.dialogs.findMany({
+            where: (
+                eq(dialogs.senderID, data.userID),
+                eq(dialogs.receiverID, data.userID)
+            ),
+        })
 
-            return dialogFromUserID
-        } catch (error) {
-            console.error('Getting dialogs error:', error)
-            throw error
+        if (!dialogFromUserID.length) {
+            throw new Error('Dialogs not found')
         }
+
+        return dialogFromUserID
     }
 
     // update
 
     async updateIsDeletedForSender(data: UpdateIsDeletedForSender): Promise<Dialog> {
-        try {
-            const dialog = await db
-                .update(dialogs)
-                .set({
-                    isDeletedForSender: data.value
-                })
-                .where(eq(dialogs.id, data.id))
-                .returning()
+        const dialog = await db
+            .update(dialogs)
+            .set({
+                isDeletedForSender: data.value
+            })
+            .where(eq(dialogs.id, data.id))
+            .returning()
 
-            return dialog[0]
-        } catch (error) {
-            console.error('Updating isDeletedForSender error:', error)
-            throw error
+        if (!dialog) {
+            throw new Error('Dialog not found')
         }
+
+        return dialog[0]
     }
 
     async updateIsDeletedForReceiver(data: UpdateIsDeletedForReceiver): Promise<Dialog> {
-        try {
-            const dialog = await db
-                .update(dialogs)
-                .set({
-                    isDeletedForReceiver: data.value
-                })
-                .where(eq(dialogs.id, data.id))
-                .returning()
+        const dialog = await db
+            .update(dialogs)
+            .set({
+                isDeletedForReceiver: data.value
+            })
+            .where(eq(dialogs.id, data.id))
+            .returning()
 
-            return dialog[0]
-        } catch (error) {
-            console.error('Updating isDeletedForReceiver error:', error)
-            throw error
+        if (!dialog) {
+            throw new Error('Dialog not found')
         }
+
+        return dialog[0]
     }
 
     async updateLastMessage(data: UpdateLastMessage): Promise<Dialog> {
-        try {
-            const dialog = await db
-                .update(dialogs)
-                .set({
-                    lastMessage: data.lastMessage
-                })
-                .where(eq(dialogs.id, data.id))
-                .returning()
+        const dialog = await db
+            .update(dialogs)
+            .set({
+                lastMessage: data.lastMessage
+            })
+            .where(eq(dialogs.id, data.id))
+            .returning()
 
-            return dialog[0]
-        } catch (error) {
-            console.error('Updating lastMessage error:', error)
-            throw error
+        if (!dialog) {
+            throw new Error('Dialog not found')
         }
+
+        return dialog[0]
     }
 
     // delete
 
     async deleteDialog(data: DeleteDialog): Promise<Dialog> {
-        try {
-            const dialog = await db
-                .delete(dialogs)
-                .where(eq(dialogs.id, data.id))
-                .returning()
+        const dialog = await db
+            .delete(dialogs)
+            .where(eq(dialogs.id, data.id))
+            .returning()
 
-            return dialog[0]
-        } catch (error) {
-            console.error('Deleting dialog error:', error)
-            throw error
+        if (!dialog) {
+            throw new Error('Dialog not found')
         }
+
+        return dialog[0]
     }
 }
 
