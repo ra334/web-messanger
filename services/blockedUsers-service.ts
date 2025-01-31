dd66ec188744d2be9e3a83a2b0d721328614eeca
ra334 <savcukmihajlo@gmail.com>
2025-01-31 11:38:04 +0200

    create blockedUsers-sservice

---
+++ services/blockedUsers-service.ts
@@ -0,0 +1,36 @@
+import { z } from 'zod'
+import blockedUsersModel from "@/db/models/blockedUsers-model";
+import {
+    BlockedUser,
+    CreateBlockedUser,
+    GetBlockdeUser,
+    GetBlockedUsers,
+    DeleteBlockedUser
+} from "@/types/blockedUsers";
+
+const idSchema = z.object({
+    id: z.string().uuid()
+})
+
+const blockedUserSchema = z.object({
+    userID: z.string().uuid()
+})
+
+class BlockedUserService {
+    async blockUser(data: CreateBlockedUser): Promise<BlockedUser> {
+        const parseData = blockedUserSchema.parse(data)
+        return await blockedUsersModel.createBlockedUser(parseData)
+    }
+
+    async getBlockedUser(data: GetBlockdeUser): Promise<BlockedUser> {
+        const parseData = idSchema.parse(data)
+        return await blockedUsersModel.getBlockedUser(parseData)
+    }
+
+    async unblockUser(data: DeleteBlockedUser): Promise<BlockedUser> {
+        const parseData = idSchema.parse(data)
+        return await blockedUsersModel.deleteBlockedUser(parseData)
+    }
+}
+
+export default new BlockedUserService()
