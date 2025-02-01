d3f79f99d714528c6215e1278ef0b54dbd6772bc
ra334 <savcukmihajlo@gmail.com>
2025-02-01 15:55:13 +0200

    refactoring

--- db/models/groupMembers-model.ts
+++ db/models/groupMembers-model.ts
@@ -22,8 +22,16 @@ class GroupMembersModel {
             
 
             return groupMember[0]
-        } catch (error) {
-            console.error('Creating group member error:', error)
+        } catch (error: any) {
+            const groupError = 'insert or update on table "groups_members" violates foreign key constraint "groups_members_group_id_groups_id_fk"'
+            const userError = 'insert or update on table "groups_members" violates foreign key constraint "groups_members_user_id_users_id_fk"'
+
+            if (error.message === groupError) {
+                throw new Error('Group not found')
+            } else if (error.message === userError) {
+                throw new Error('User not found')
+            }
+            
             throw error
         }
     }
@@ -31,67 +39,60 @@ class GroupMembersModel {
     // read
 
     async getGroupMember(data: GetGroupMember): Promise<GroupMembers> {
-        try {
-            const groupMember = await db.query.groupsMembers.findFirst({
-                where: (eq(groupsMembers.id, data.id))
-            })
+        const groupMember = await db.query.groupsMembers.findFirst({
+            where: (eq(groupsMembers.id, data.id))
+        })
 
-            if (!groupMember) {
-                throw new Error('Group member not found')
-            }
-
-            return groupMember
-        } catch (error) {
-            console.error('Getting group member error:', error)
-            throw error
+        if (!groupMember) {
+            throw new Error('Group member not found')
         }
+
+        return groupMember
     }
 
-    async getGroupMembers(data: GetGroupMembers) {
-        try {
-            const groupMembers = await db.query.groupsMembers.findMany({
-                where: (eq(groupsMembers.groupID, data.groupID)),
-                limit: 10
-            })
+    async getGroupMembers(data: GetGroupMembers): Promise<GroupMembers[]> {
+        const groupMembers = await db.query.groupsMembers.findMany({
+            where: (eq(groupsMembers.groupID, data.groupID))
+        })
 
-            return groupMembers
-        } catch (error) {
-            console.error('Getting group members error:', error)
-            throw error
+        if (!groupMembers.length) {
+            throw new Error('Group members not found')
         }
+
+        return groupMembers
     }
 
     // update
 
-    async updateGroupIsAdmin(data: UpdateGroupIsAdmin) {
-        try {
-            const groupMember = await db
-                .update(groupsMembers)
-                .set({
-                    isAdmin: data.isAdmin
-                })
-                .where(eq(groupsMembers.id, data.id))
-                .returning()
+    async updateGroupIsAdmin(data: UpdateGroupIsAdmin): Promise<GroupMembers> {
+        const groupMember = await db
+            .update(groupsMembers)
+            .set({
+                isAdmin: data.isAdmin
+            })
+            .where(eq(groupsMembers.id, data.id))
+            .returning()
 
-            return groupMember[0]
-        } catch (error) {
-            console.error('Updating group member error:', error)
-            throw error
+        if (!groupMember.length) {
+            throw new Error('Group member not found')
         }
+
+        return groupMember[0]
     }
 
     // delete
 
     async deleteGroupMember(data: DeleteGroupMember) {
-        try {
-            const gorupMember = await db
-                .delete(groupsMembers)
-                .where(eq(groupsMembers.id, data.id))
-                .returning()
-        } catch (error) {
-            console.error('Deleting group member error:', error)
-            throw error
+        const gorupMember = await db
+            .delete(groupsMembers)
+            .where(eq(groupsMembers.id, data.id))
+            .returning()
+
+        if (!gorupMember.length) {
+            throw new Error('Group member not found')
         }
+
+        return gorupMember[0]
     }
 }
 
