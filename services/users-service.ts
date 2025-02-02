9e5c53a0aaaa40ce5b655cbd90cc12264439e80f
ra334 <savcukmihajlo@gmail.com>
2025-02-02 12:17:26 +0200

    validate data in users-service

--- services/users-service.ts
+++ services/users-service.ts
@@ -1,3 +1,4 @@
+import { z } from 'zod'
 import usersModel from '@/db/models/users-model'
 import 'dotenv/config'
 import bcrypt from 'bcrypt'
@@ -21,200 +22,277 @@ import {
     LoginUser
 } from '@/types/users'
 
+const idSchema = z.object({
+    id: z.string().uuid()
+})
+
+const emailSchema = z.object({
+    email: z.string().email().max(255)
+})
+
+const nickNameSchema = z.object({
+    nickName: z.string().max(50)
+})
+
+const passwordSchema = z.object({
+    password: z.string().min(8)
+})
+
+const avatarURLSchema = z.object({
+    avatarURL: z.string().max(255)
+})
+
+const registerUserSchema = z.object({
+    ...nickNameSchema.shape,
+    ...emailSchema.shape,
+    password: z.string().min(8).nullable(),
+    ...avatarURLSchema.shape
+})
+
+const loginWithCredentialsSchema = z.object({
+    nickName: z.string().max(50).nullable(),
+    email: z.string().email().max(255).nullable(),
+    ...passwordSchema.shape
+})
+
+const updateUserNicknameSchema = z.object({
+    ...idSchema.shape,
+    ...nickNameSchema.shape
+})
+
+const updateUserEmailSchema = z.object({
+    ...idSchema.shape,
+    ...emailSchema.shape
+})
+
+const updateUserPasswordSchema = z.object({
+    ...idSchema.shape,
+    ...passwordSchema.shape
+})
+
+const updateUserAvatarURLSchema = z.object({
+    ...idSchema.shape,
+    ...avatarURLSchema.shape
+})
+
+const updateUserStatusSchema = z.object({
+    ...idSchema.shape,
+    status: z.enum(['offline', 'online', 'away'])
+})
+
+const updateUserIsVerifiedSchema = z.object({
+    ...idSchema.shape,
+    isVerified: z.boolean()
+})
+
+const updateUserIsReportedSchema = z.object({
+    ...idSchema.shape,
+    isReported: z.boolean()
+})
+
+const updateUserIsActiveSchema = z.object({
+    ...idSchema.shape,
+    isActive: z.boolean()
+})
+
+const updateUserIsBlockedSchema = z.object({
+    ...idSchema.shape,
+    isBlocked: z.boolean()
+})
 
 class UsersService {
-    #nickNameValidation(nickname: string) {
-        if (!nickname) {
-            throw new Error('Nickname is required')
+    private handleValidationError(error: unknown): never {
+        if (error instanceof z.ZodError) {
+            throw new Error(error.errors[0].message)
         }
-    
-        if (nickname.length > 50) {
-            throw new Error('Nickname is too long')
-        }
-    
-        return true
+
+        throw error
     }
-    
-    #emailValidation(email: string) {
-        const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/
-    
-        if (!email) {
-            throw new Error('Email is required')
-        }
-    
-        if (!emailRegex.test(email)) {
-            throw new Error('Invalid email')
-        }
-    
-        if (email.length > 255) {
-            throw new Error('Email is too long')
-        }
-    
-        return true
-    }
-    
-    #passwordValidation(password: string) {
-        if (!password) {
-            throw new Error('Password is required')
-        }
-    
-        if (password.length < 8) {
-            throw new Error('Password is too short')
-        }
-    
-        return true
-    }
-    
-    #avatarURLValidation(avatarURL: string) {
-        if (!avatarURL) {
-            throw new Error('Avatar URL is required')
-        }
-    
-        if (avatarURL.length > 255) {
-            throw new Error('Avatar URL is too long')
-        }
-    
-        return true
-    }
-    
+
     async registerUser(data: CreateUser): Promise<UserData> {
-        let user: UserData
-        const saltRounds = Number(process.env.SALT_ROUNDS)
-    
-        this.#nickNameValidation(data.nickName)
-        this.#emailValidation(data.email)
-        this.#avatarURLValidation(data.avatarURL)
-    
-        if (data.password) {
-            this.#passwordValidation(data.password)
-            const hashedPassword = await bcrypt.hash(data.password, saltRounds)
-            user = await usersModel.createUser({
-                nickName: data.nickName,
-                email: data.email,
-                password: hashedPassword,
-                avatarURL: data.avatarURL
+        try {
+            const saltRounds = Number(process.env.SALT_ROUNDS)
+            const parseData = registerUserSchema.parse(data)
+        
+            const passwordHah = data.password 
+                ? await bcrypt.hash(data.password, saltRounds)
+                : null
+
+            return await usersModel.createUser({
+                ...parseData,
+                password: passwordHah
             })
 
-            return user
+        } catch (error) {
+            this.handleValidationError(error)
         }
-    
-        user = await usersModel.createUser({
-            nickName: data.nickName,
-            email: data.email,
-            password: '',
-            avatarURL: data.avatarURL
-        })
-    
-        return user
     }
-    
-    async loginUser(data: LoginUser): Promise<UserData> {
-        let user: UserData
-        let comparePassword = false
-    
-        if (data.nickName) {
-            this.#nickNameValidation(data.nickName)
-            user = await usersModel.getUserByNickname({nickName: data.nickName})
-        } else {
-            this.#emailValidation(data.email)
-            user = await usersModel.getUserByEmail({email: data.email})
-        }
-    
-        if (!user.password) {
-            throw new Error("User doesn't have a password")
-        }
 
-        comparePassword = await bcrypt.compare(data.password, user.password)
+    async loginWithCredentials(data: LoginUser) {
+        try {
+            const parseData = loginWithCredentialsSchema.parse(data)
+            let user: UserData | null = null
+        
+            if (parseData.nickName) {
+                user = await usersModel.getUserByNickname({nickName: parseData.nickName})
+            } else if (parseData.email) {
+                user = await usersModel.getUserByEmail({email: parseData.email})
+            }
 
-        if (!comparePassword) {
-            throw new Error('Invalid password')
+            if (!user) {
+                throw new Error('User not found')
+            }
+
+            if (user.password === null) {
+                throw new Error('User has no password')
+            }
+
+            const isPasswordValid = await bcrypt.compare(parseData.password, user.password)
+
+            if (!isPasswordValid) {
+                throw new Error('Invalid password')
+            }
+
+            return user
+        } catch (error) {
+            this.handleValidationError(error)
         }
+    }
 
-        return user
+    async loginWithOauth() {
+
     }
 
     async getUser(data: GetUser): Promise<UserData> {
-        const user = await usersModel.getUser(data)
-
-        return user
+        try {
+            const parseData = idSchema.parse(data)
+            return await usersModel.getUser(parseData)
+        } catch (error) {
+            this.handleValidationError(error)
+        }
     }
 
     async getUserAvatarURL(data: GetUser): Promise<ReturnUserAvatarURL> {
-        const avatarURL = await usersModel.getUserAvatarURL(data)
-
-        return avatarURL
+        try {
+            const parseData = idSchema.parse(data)
+            return await usersModel.getUserAvatarURL(parseData)
+        } catch (error) {
+            this.handleValidationError(error)
+        }
     }
 
     async getUserByEmail(data: GetUserByEmail): Promise<UserData> {
-        const user = await usersModel.getUserByEmail(data)
-
-        return user
+        try {
+            const parseData = emailSchema.parse(data)
+            return await usersModel.getUserByEmail(parseData)
+        } catch (error) {
+            this.handleValidationError(error)
+        }
     }
 
     async getUserByNickname(data: GetUserByNickname): Promise<UserData> {
-        const user = await usersModel.getUserByNickname(data)
-
-        return user
+        try {
+            const parseData = nickNameSchema.parse(data)
+            return await usersModel.getUserByNickname(parseData)
+        } catch (error) {
+            this.handleValidationError(error)
+        }
     }
 
     async updateUserNickname(data: UpdateUserNickname): Promise<UserData> {
-        const user = await usersModel.updateUserNickname(data)
-
-        return user
+        try {
+            const parseData = updateUserNicknameSchema.parse(data)
+            return await usersModel.updateUserNickname(parseData)
+        } catch (error) {
+            this.handleValidationError(error)
+        }
     }
 
     async updateUserEmail(data: UpdateUserEmail): Promise<UserData> {
-        const user = await usersModel.updateUserEmail(data)
-
-        return user
+        try {
+            const parseData = updateUserEmailSchema.parse(data)
+            return await usersModel.updateUserEmail(parseData)
+        } catch (error) {
+            this.handleValidationError(error)
+        }
     }
 
     async updateUserPassword(data: UpdateUserPassword): Promise<UserData> {
-        const user = await usersModel.updateUserPassword(data)
+        try {
+            const saltRounds = Number(process.env.SALT_ROUNDS)
+            const parseData = updateUserPasswordSchema.parse(data)
+            const passwordHah = await bcrypt.hash(data.password, saltRounds)
 
-        return user
+            return await usersModel.updateUserPassword({
+                id: parseData.id,
+                password: passwordHah
+            })
+        } catch (error) {
+            this.handleValidationError(error)
+        }
     }
 
     async updateUserAvatarURL(data: UpdateUserAvatarURL): Promise<UserData> {
-        const user = await usersModel.updateUserAvatarURL(data)
-
-        return user
+        try {
+            const parseData = updateUserAvatarURLSchema.parse(data)
+            return await usersModel.updateUserAvatarURL(parseData)
+        } catch (error) {
+            this.handleValidationError(error)
+        }
     }
 
     async updateUserStatus(data: UpdateUserStatus): Promise<UserData> {
-        const user = await usersModel.updateUserStatus(data)
-
-        return user
+        try {
+            const parseData = updateUserStatusSchema.parse(data)
+            return await usersModel.updateUserStatus(parseData)
+        } catch (error) {
+            this.handleValidationError(error)
+        }
     }
 
     async updateUserIsVerified(data: UpdateUserIsVerified): Promise<UserData> {
-        const user = await usersModel.updateUserIsVerified(data)
-
-        return user
+        try {
+            const parseData = updateUserIsVerifiedSchema.parse(data)
+            return await usersModel.updateUserIsVerified(parseData)
+        } catch (error) {
+            this.handleValidationError(error)
+        }
     }
 
     async updateUserIsReported(data: UpdateUserIsReported): Promise<UserData> {
-        const user = await usersModel.updateUserIsReported(data)
-
-        return user
+        try {
+            const parseData = updateUserIsReportedSchema.parse(data)
+            return await usersModel.updateUserIsReported(parseData)
+        } catch (error) {
+            this.handleValidationError(error)
+        }
     }
 
     async updateUserIsActive(data: UpdateUserIsActive): Promise<UserData> {
-        const user = await usersModel.updateUserIsActive(data)
-
-        return user
+        try {
+            const parseData = updateUserIsActiveSchema.parse(data)
+            return await usersModel.updateUserIsActive(parseData)
+        } catch (error) {
+            this.handleValidationError(error)
+        }
     }
 
     async updateUserIsBlocked(data: UpdateUserIsBlocked): Promise<UserData> {
-        const user = await usersModel.updateUserIsBlocked(data)
-
-        return user
+        try {
+            const parseData = updateUserIsBlockedSchema.parse(data)
+            return await usersModel.updateUserIsBlocked(parseData)
+        } catch (error) {
+            this.handleValidationError(error)
+        }
     }
 
     async deleteUser(data: DeleteUser): Promise<UserData> {
-        const user = await usersModel.deleteUser(data)
-
-        return user
+        try {
+            const parseData = idSchema.parse(data)
+            return await usersModel.deleteUser(parseData)
+        } catch (error) {
+            this.handleValidationError(error)
+        }
     }
 }
 
