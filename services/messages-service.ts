642f09942da08b7d057c8da4a755973deb1c8c04
ra334 <savcukmihajlo@gmail.com>
2025-02-02 11:04:45 +0200

    add zod for data validate

--- services/messages-service.ts
+++ services/messages-service.ts
@@ -1,3 +1,4 @@
+import { z } from 'zod'
 import messagesModel from "@/db/models/messages-model";
 import type {
     Message,
@@ -11,54 +12,157 @@ import type {
     DeleteMessage
 } from '@/types/messages'
 
+
+const idSchema = z.object({
+    id: z.string().uuid()
+})
+
+const dialogIDSchema = z.object({
+    dialogID: z.string().uuid()
+})
+
+const createMessageSchema = z.object({
+    dialogID: z.string().uuid().nullable(),
+    groupID: z.string().uuid().nullable(),
+    senderID: z.string().uuid(),
+    messageType: z.enum(['text', 'media']),
+    mediaID: z.string().nullable(),
+    text: z.string().nullable()
+})
+
+const getMessageFromUser = z.object({
+    userID: z.string().uuid(),
+    ...dialogIDSchema.shape
+})
+
+const updateMessageTextSchema = z.object({
+    ...idSchema.shape,
+    text: z.string()
+})
+
+const updateMessageIsEditedSchema = z.object({
+    ...idSchema.shape,
+    isEdited: z.boolean()
+})
+
+const updateMessageIsReadedSchema = z.object({
+    ...idSchema.shape,
+    isReaded: z.boolean()
+})
+
+
 class MessagesService {
     
     async sendMessage(data: CreateMessage): Promise<Message> {
-        const message = await messagesModel.createMessage(data)
+        try {
+            const parseData = createMessageSchema.parse(data)
+            return await messagesModel.createMessage(parseData)
+        } catch (error: any) {
 
-        return message
+            if (error instanceof z.ZodError) {
+                throw new Error(error.errors[0].message)
+            }
+
+            throw error
+        }
     }
 
     async getMessage(data: GetMessage): Promise<Message> {
-        const message = await messagesModel.getMessage(data)
+        try {
+            const parseData = idSchema.parse(data)
+            return await messagesModel.getMessage(parseData)
+        } catch (error: any) {
 
-        return message
+            if (error instanceof z.ZodError) {
+                throw new Error(error.errors[0].message)
+            }
+
+            throw error
+        }
     }
 
     async getMessagesFromDialog(data: GetMessagesFromDialog): Promise<Message[]> {
-        const messages = await messagesModel.getMessagesFromDialog(data)
+        try {
+            const parseData = dialogIDSchema.parse(data)
+            return await messagesModel.getMessagesFromDialog(parseData)
+        } catch (error: any) {
+            
+            if (error instanceof z.ZodError) {
+                throw new Error(error.errors[0].message)
+            }
 
-        return messages
+            throw error
+        }
     }
 
     async getMessagesFromUser(data: GetMessagesFromUser): Promise<Message[]> {
-        const messages = await messagesModel.getMessagesFromUser(data)
+        try {
+            const parseData = getMessageFromUser.parse(data)
+            return await messagesModel.getMessagesFromUser(parseData)
+        } catch (error: any) {
+            
+            if (error instanceof z.ZodError) {
+                throw new Error(error.errors[0].message)
+            }
 
-        return messages
+            throw error
+        }
     }
 
     async updateMessageText(data: UpdateMessageText): Promise<Message> {
-        const message = await messagesModel.updateMessageText(data)
+        try {
+            const parseData = updateMessageTextSchema.parse(data)
+            return await messagesModel.updateMessageText(parseData)
+        } catch (error: any) {
+            
+            if (error instanceof z.ZodError) {
+                throw new Error(error.errors[0].message)
+            }
 
-        return message
+            throw error
+        }
     }
 
     async updateMessageIsEdited(data: UpdateMessageIsEdited): Promise<Message> {
-        const message = await messagesModel.updateMessageIsEdited(data)
+        try {
+            const parseData = updateMessageIsEditedSchema.parse(data)
+            return await messagesModel.updateMessageIsEdited(parseData)
+        } catch (error: any) {
+            
+            if (error instanceof z.ZodError) {
+                throw new Error(error.errors[0].message)
+            }
 
-        return message
+            throw error
+        }
     }
 
     async updateMessageIsReaded(data: UpdateMessageIsReaded): Promise<Message> {
-        const message = await messagesModel.updateMessageIsReaded(data)
+        try {
+            const parseData = updateMessageIsReadedSchema.parse(data)
+            return await messagesModel.updateMessageIsReaded(parseData)
+        } catch (error: any) {
+            
+            if (error instanceof z.ZodError) {
+                throw new Error(error.errors[0].message)
+            }
 
-        return message
+            throw error
+        }
     }
 
     async deleteMessage(data: DeleteMessage): Promise<Message> {
-        const message = await messagesModel.deleteMessage(data)
+        try {
+            const parseData = idSchema.parse(data)
+            return await messagesModel.deleteMessage(parseData)
+        } catch (error: any) {
+            
+            if (error instanceof z.ZodError) {
+                throw new Error(error.errors[0].message)
+            }
 
-        return message
+            throw error
+        }
     }
 }
 
