2933dc641afc682de65252e44a00c0249c6ebbee
ra334 <savcukmihajlo@gmail.com>
2025-02-04 16:43:26 +0200

    refactoring

--- db/models/userVerifications-model.ts
+++ db/models/userVerifications-model.ts
@@ -31,76 +31,64 @@ class UserVerifivationsModel {
     // read
     
     async getUserVerification(data: GetUserVerification): Promise<UsesrVerification> {
-        try {
-            const userVerification = await db.query.userVerifications.findFirst({
-                where: (eq(userVerifications.userID, data.userID))
-            })
-    
-            if (!userVerification) {
-                throw new Error('User verification not found')
-            }
-    
-            return userVerification
-    
-        } catch (error) {
-            console.error('Getting userverification error:', error)
-            throw error;
+        const userVerification = await db.query.userVerifications.findFirst({
+            where: (eq(userVerifications.userID, data.userID))
+        })
+
+        if (!userVerification) {
+            throw new Error('User verification not found')
         }
+
+        return userVerification
     }
     
     // update
     
     async updateVerificationCode(data: UpdateUserVerification): Promise<UsesrVerification> {
-        try {
-            const userVerification = await db
-                .update(userVerifications)
-                .set({
-                    verificationCode: data.verificationCode
-                })
-                .where(eq(userVerifications.userID, data.userID))
-                .returning()
-    
-            return userVerification[0]
-    
-        } catch (error) {
-            console.error('Updating userverification code error:', error)
-            throw error;
-        }
+        const userVerification = await db
+            .update(userVerifications)
+            .set({
+                verificationCode: data.verificationCode
+            })
+            .where(eq(userVerifications.userID, data.userID))
+            .returning()
+
+        if (!userVerification.length) {
+            throw new Error('User verification not found')
+        } 
+
+        return userVerification[0]
     }
     
     async updateIsUsed(data: UpdateIsUsed): Promise<UsesrVerification> {
-        try {
-            const userVerification = await db
-                .update(userVerifications)
-                .set({
-                    isUsed: data.isUsed
-                })
-                .where(eq(userVerifications.userID, data.userID))
-                .returning()
-    
-            return userVerification[0]
-    
-        } catch (error) {
-            console.error('Updating userverification isUsed error:', error)
-            throw error;
+        const userVerification = await db
+            .update(userVerifications)
+            .set({
+                isUsed: data.isUsed
+            })
+            .where(eq(userVerifications.userID, data.userID))
+            .returning()
+
+        if (!userVerification.length) {
+            throw new Error('User verification not found')
         }
+
+        return userVerification[0]
     }
     
     // delete
     
     async deleteUserVerification(data: DeleteUserVerification): Promise<UsesrVerification> {
-        try {
-            const userVerification = await db
-                .delete(userVerifications)
-                .where(eq(userVerifications.userID, data.userID))
-                .returning()
-    
-            return userVerification[0]
-    
-        } catch (error) {
-            console.error('Deleting userverification error:', error)
-            throw error;
+        const userVerification = await db
+            .delete(userVerifications)
+            .where(eq(userVerifications.userID, data.userID))
+            .returning()
+
+        if (!userVerification.length) {
+            throw new Error('User verification not found')
         }
+
+        return userVerification[0]
     }
 }
 
