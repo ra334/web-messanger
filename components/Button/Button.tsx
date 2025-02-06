2a1093beb19cb039fd8a92cf144f0a701e4236e3
ra334 <savcukmihajlo@gmail.com>
2025-02-06 11:05:16 +0200

    create button component

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,5 +1,23 @@
-function Button() {
-    return <button></button>
+interface ButtonProps {
+    children: React.ReactNode;
+    onClick?: () => void;
+}
+
+function Button({children, onClick}: ButtonProps) {
+    return (
+        <button className="
+            w-full
+            text-center
+            border-2
+          border-black
+            rounded-[20px]
+            font-normal
+            text-xl
+            py-2
+        " onClick={onClick}>
+            {children}
+        </button>
+    )
 }
 
 export default Button
