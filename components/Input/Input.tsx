f53ac29c2c1788307ca88af1306edf19e07d9737
ra334 <savcukmihajlo@gmail.com>
2025-02-06 11:05:29 +0200

    create input component

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -1,5 +1,22 @@
-function Input() {
-    return <input type="text" />
+interface InputProps {
+    placeholder?: string;
+    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+function Input({placeholder, onChange}: InputProps) {
+    return (
+        <input className="
+            w-full
+            border-2  
+          border-black
+            rounded-[20px]
+            font-normal
+            px-5
+            py-[8px]
+            placeholder:text-black
+            bg-transparent
+        " placeholder={placeholder} onChange={onChange} type="text" />
+    )
 }
 
 export default Input
