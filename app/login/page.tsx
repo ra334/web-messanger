8500d76c83ca176c1b1aff47cda45153f3a47c03
msavchuk <savcukmihajlo@gmail.com>
2025-04-03 21:30:45 +0300

    refactor: split into separate login and registration pages

---
+++ app/login/page.tsx
@@ -0,0 +1,13 @@
+import Login from "@/components/Login/Login"
+
+function LoginPage() {
+    return (
+        <div className="w-full h-full flex justify-center items-center">
+            <div className="max-w-[450px] w-full">
+                <Login />
+            </div>
+        </div>
+    )
+}
+
+export default LoginPage
---
+++ app/registration/page.tsx
@@ -0,0 +1,13 @@
+import Signup from "@/components/Signup/Signup"
+
+function SignupPage() {
+    return (
+        <div className="w-full h-full flex justify-center items-center">
+            <div className="max-w-[450px] w-full">
+                <Signup />
+            </div>
+        </div>
+    )
+}
+
+export default SignupPage
